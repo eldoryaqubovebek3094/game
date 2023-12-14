056ef1731411c4164341b3fbdbee23dadf1a3761
unknown <you@example.com>
2023-12-14 21:10:46 +0300

    loyiha

---
+++ index.js
@@ -0,0 +1,102 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const { gameOptions, againOptions } = require("./options");
+
+const token = "6668772112:AAFy3ji3xJCY3UYQVJmHNxZ6lHwFrUTfZdw";
+
+const bot = new TelegramBot(token, { polling: true });
+
+const obj = {};
+
+const startGame = async (chatId) => {
+  await bot.sendSticker(chatId, "./images/game-removebg-preview.png");
+
+  await bot.sendMessage(
+    chatId,
+    `O'yinimiz sharti shundan iboratki, Kompyuter 0️⃣ dan 9️⃣ gacha son o'yladi,Siz shu sonni topishga harakat qiling `
+  );
+  const randomNumber = Math.floor(Math.random() * 10); // 0 da 9 gacha son chaqiradi
+  obj[chatId] = randomNumber;
+  await bot.sendMessage(chatId, "To'g'ri sonni toping ⬇", gameOptions);
+};
+
+const bootstrap = () => {
+  bot.setMyCommands([
+    {
+      command: "/start",
+      description: "Botni qayta ishga tushirish🔁",
+    },
+    {
+      command: "/info",
+      description: "O'zingiz haqida ma'lumot💡",
+    },
+    {
+      command: "/game",
+      description: "O'yin Boshlash🎮",
+    },
+  ]);
+
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    console.log(msg);
+    const chatId = msg.chat.id;
+    if (text === "/start") {
+      await bot.sendSticker(chatId, "./images/4712109.png");
+      return bot.sendMessage(
+        chatId,
+        `Assalomu Alaykum Hurmatli ➡${msg.from?.first_name}⬅Sizni ushbu botimizda ko'rib turganimizdan hursandmiz✅`
+      );
+    }
+    if (text === "/info") {
+      await bot.sendSticker(chatId, "./images/4712029.png");
+      return bot.sendMessage(
+        chatId,
+        `Sizning telegram UserName ${msg.from?.username}, Sizning Ismingiz ${msg.from?.first_name}, Familiyangiz ${msg.from?.last_name}, va Sizning Id esa ${msg.from?.id}✅:`
+      );
+    }
+
+    if (text === "/game") {
+      return startGame(chatId);
+    }
+
+    await bot.sendSticker(chatId, "./images/error.webp");
+    bot.sendMessage(
+      chatId,
+      `Uzr xatolik ❌ bo'ldi Hurmatli ${msg.from?.first_name}✅`
+    );
+  });
+
+  bot.on("callback_query", async (msg) => {
+    console.log(msg);
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+
+    // bot.on("callback_query", async (callback_query) => {
+    //   console.log(callback_query);
+    //   const data = callback_query.data;
+    //   const chatId = callback_query.message.chat.id;
+
+    if (data === "/again") {
+      return startGame(chatId);
+    }
+
+    await bot.sendMessage(chatId, `Siz ${data} ni Tanladingiz`);
+    if (data == obj[chatId]) {
+      await bot.sendSticker(chatId, "./images/salyut.gif");
+      await bot.sendMessage(
+        chatId,
+        `Siz tog'ri javob berdingiz. Kompyuter ${obj[chatId]} sonni tanlagan edi`,
+        againOptions
+      );
+    } else {
+      await bot.sendSticker(chatId, "./images/game-over-removebg-preview.png");
+      await bot.sendMessage(
+        chatId,
+        `Siz tanlagan ${data} soni noto'g'ri. Kompyuter ${obj[chatId]} sonini tanlagan edi `,
+        againOptions
+      );
+    }
+  });
+};
+
+bootstrap();
---
+++ options.js
@@ -0,0 +1,69 @@
+module.exports = {
+  gameOptions: {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "1️⃣",
+            callback_data: "1",
+          },
+          {
+            text: "2️⃣",
+            callback_data: "2",
+          },
+          {
+            text: "3️⃣",
+            callback_data: "3",
+          },
+        ],
+        [
+          {
+            text: "4️⃣",
+            callback_data: "4",
+          },
+          {
+            text: "5️⃣",
+            callback_data: "5",
+          },
+          {
+            text: "6️⃣",
+            callback_data: "6",
+          },
+        ],
+        [
+          {
+            text: "7️⃣",
+            callback_data: "7",
+          },
+          {
+            text: "8️⃣",
+            callback_data: "8",
+          },
+          {
+            text: "9️⃣",
+            callback_data: "9",
+          },
+        ],
+        [
+          {
+            text: "0️⃣",
+            callback_data: "0",
+          },
+        ],
+      ],
+    },
+  },
+  againOptions: {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "➡Qaytadan Boshlash🔁",
+            callback_data: "/again",
+          },
+        ],
+      ],
+    },
+  },
+  
+};
